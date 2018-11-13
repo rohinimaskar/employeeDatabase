4211876b755ab14acbc1594d0ee5f8f2af411f54
rohinimaskar <rohinimaskar2@gmail.com>
2018-11-12 22:29:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ if (req.baseUrl && req.baseUrl.indexOf('/api') > -1) {
 require('./app/routes/employee.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(process.env.PORT ||3000, () => {
     console.log("Server is listening on port 3000");
 });
